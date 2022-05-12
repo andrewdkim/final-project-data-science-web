483e026448663504414dfb5faa0b1ab5894b0a88
andrewdkim <adkim414@gmail.com>
2022-05-11 23:34:59 -0400

    remove artifacts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import { Box, Container } from "@mui/material";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Menu from "./organisms/Menu";
 import Main from "./pages/Main";
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createStore } from 'redux';
 import rootReducer from './store';
 import { Provider } from 'react-redux';
-import { UncountableData } from './api/UncountableData';
 
 const store = createStore(rootReducer)
-UncountableData.parseData();
 
 ReactDOM.render(
   <React.StrictMode>
